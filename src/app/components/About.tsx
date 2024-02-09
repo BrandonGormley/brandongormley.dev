2e5801f8d0326497514602590cacb841a92ee3cd
Brandon Gormley <brandontgormley@gmail.com>
2024-02-09 13:30:18 -0600

    update styling & add social links

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,4 +1,11 @@
 import Image from 'next/image';
+import {
+    IoLogoLinkedin,
+    IoLogoGithub,
+    IoLogoInstagram,
+    IoLogoYoutube,
+    IoLogoTwitter,
+} from 'react-icons/io5';
 
 export default function About() {
     return (
@@ -13,17 +20,49 @@ export default function About() {
                 />
 
                 <div className='flex flex-col w-full my-8 md:p-8 lg:p-16 md:justify-center'>
-                    <p className='text-2xl font-light tracking-tighter my-2 lg:text-4xl leading-loose'>
+                    <p className='text-2xl font-light tracking-wide lg:my-2 lg:text-4xl leading-loose'>
                         Hello World! I&apos;m Brandon ☕️
                     </p>
-                    <p className='text-gray-800 text-4xl font-bold lg:text-5xl leading-loose'>
+                    <p className='text-gray-800 text-3xl font-bold lg:my-2 lg:text-5xl xl:text-6xl leading-loose'>
                         Front End Developer
                     </p>
-                    <p className='text-gray-800 text-lg my-8 font-light leading-loose large:text-xl'>
+                    <p className='text-gray-800 text-lg font-light lg:my-4 leading-loose large:text-xl'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit. Ea provident accusantium eveniet, blanditiis
                         repudiandae minus quis quia ab consequatur harum.
                     </p>
+                    <div className='flex my-6'>
+                        <a
+                            href='https://www.linkedin.com/in/brandontgormley/'
+                            className='mx-2'
+                        >
+                            <IoLogoLinkedin className='hover:text-[#0077b5] text-gray-400 h-7 w-7 duration-200 transition-all' />
+                        </a>
+                        <a
+                            href='https://www.instagram.com/brandongormley'
+                            className='mx-2'
+                        >
+                            <IoLogoInstagram className='hover:text-[#d62976] text-gray-400 h-7 w-7 duration-200 transition-all' />
+                        </a>
+                        <a
+                            href='https://www.youtube.com/channel/UCOtIOhA9Y0a36ah9UUZ6RCw'
+                            className='mx-2'
+                        >
+                            <IoLogoYoutube className='hover:text-[#FF0000] text-gray-400 h-7 w-7 duration-200 transition-all' />
+                        </a>
+                        <a
+                            href='https://twitter.com/BrandonGormley'
+                            className='mx-2'
+                        >
+                            <IoLogoTwitter className='hover:text-[#1DA1F2] text-gray-400 h-7 w-7 duration-200 transition-all' />
+                        </a>
+                        <a
+                            href='https://github.com/BrandonGormley'
+                            className='mx-2'
+                        >
+                            <IoLogoGithub className='hover:text-gray-900 text-gray-400 h-7 w-7 duration-200 transition-all' />
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
