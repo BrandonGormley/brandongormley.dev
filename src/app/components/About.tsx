6f8bc9866fa687cb7df9c9af72044407fbf9fc79
Brandon Gormley <brandontgormley@gmail.com>
2024-02-10 12:44:49 -0600

    update styling

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,16 +1,10 @@
 import Image from 'next/image';
-import {
-    IoLogoLinkedin,
-    IoLogoGithub,
-    IoLogoInstagram,
-    IoLogoYoutube,
-    IoLogoTwitter,
-} from 'react-icons/io5';
+import SocialBar from '@/app/components/ui/SocialBar';
 
 export default function About() {
     return (
         <section className='w-full mx-auto max-w-[1280px]'>
-            <div className='flex flex-col my-16 justify-center mx-auto md:mx-0 md:flex-row md:justify-between max-w-[1280px] px-4 transition-all'>
+            <div className='flex flex-col my-12 justify-center mx-auto md:mx-0 md:flex-row md:justify-between max-w-[1280px] px-4 transition-all'>
                 <Image
                     src={'/hero.jpeg'}
                     width={500}
@@ -31,38 +25,7 @@ export default function About() {
                         elit. Ea provident accusantium eveniet, blanditiis
                         repudiandae minus quis quia ab consequatur harum.
                     </p>
-                    <div className='flex my-6'>
-                        <a
-                            href='https://www.linkedin.com/in/brandontgormley/'
-                            className='mx-2'
-                        >
-                            <IoLogoLinkedin className='hover:text-[#0077b5] text-gray-400 h-7 w-7 duration-200 transition-all' />
-                        </a>
-                        <a
-                            href='https://www.instagram.com/brandongormley'
-                            className='mx-2'
-                        >
-                            <IoLogoInstagram className='hover:text-[#d62976] text-gray-400 h-7 w-7 duration-200 transition-all' />
-                        </a>
-                        <a
-                            href='https://www.youtube.com/channel/UCOtIOhA9Y0a36ah9UUZ6RCw'
-                            className='mx-2'
-                        >
-                            <IoLogoYoutube className='hover:text-[#FF0000] text-gray-400 h-7 w-7 duration-200 transition-all' />
-                        </a>
-                        <a
-                            href='https://twitter.com/BrandonGormley'
-                            className='mx-2'
-                        >
-                            <IoLogoTwitter className='hover:text-[#1DA1F2] text-gray-400 h-7 w-7 duration-200 transition-all' />
-                        </a>
-                        <a
-                            href='https://github.com/BrandonGormley'
-                            className='mx-2'
-                        >
-                            <IoLogoGithub className='hover:text-gray-900 text-gray-400 h-7 w-7 duration-200 transition-all' />
-                        </a>
-                    </div>
+                    <SocialBar />
                 </div>
             </div>
         </section>
