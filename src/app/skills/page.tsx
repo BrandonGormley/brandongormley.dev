03cc45da00a093f8d9f5dc4b13a120356fdcc7a6
Brandon Gormley <brandontgormley@gmail.com>
2024-02-11 12:01:42 -0600

    update skills styling

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -6,6 +6,7 @@ import {
     IoLogoReact,
     IoLogoNodejs,
     IoGitMerge,
+    IoLogoFigma,
 } from 'react-icons/io5';
 import {
     SiExpress,
@@ -23,12 +24,13 @@ import {
 export default function page() {
     return (
         <section className='w-full mx-auto max-w-[1280px]'>
-            <div className='flex flex-col my-12 justify-center mx-auto md:mx-0 max-w-[1280px] px-4 transition-all w-full'>
+            <div className='flex flex-col my-12 mx-auto md:mx-0 max-w-[1280px] px-4 transition-all w-full'>
                 <div className='flex flex-col w-full mt-8 md:p-8 md:justify-center lg:max-w-[65%]'>
-                    <p className='text-2xl font-light tracking-wide lg:my-2 lg:text-4xl leading-loose'>
+                    <p className='text-3xl font-light tracking-wide lg:my-2 lg:text-4xl leading-loose'>
                         Skills
                     </p>
-                    <p className='text-gray-800 text-md font-light lg:my-4 leading-loose large:text-xl'>
+                    <hr className='mb-4 w-40' />
+                    <p className='text-gray-800 text-md font-light leading-loose large:text-xl'>
                         As a passionate Next.js developer deeply immersed in the
                         React ecosystem, I prioritize crafting seamless user
                         experiences. With a keen understanding of UI/UX
@@ -42,24 +44,24 @@ export default function page() {
                         trends.
                     </p>
                 </div>
-                <div className='flex my-4 md:p-8'>
-                    <IoLogoHtml5 className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <IoLogoCss3 className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <IoLogoJavascript className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <IoLogoSass className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <SiTailwindcss className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <SiTypescript className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <IoLogoReact className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <SiRedux className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />{' '}
-                    <IoLogoNodejs className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <SiExpress className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <SiNextdotjs className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <SiMongodb className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <SiSupabase className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <SiWebpack className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <SiPostcss className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <SiGithub className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
-                    <IoGitMerge className=' text-gray-400 h-7 w-7 duration-200 transition-all mx-1' />
+                <div className='flex flex-wrap my-6 md:pl-6'>
+                    <IoLogoHtml5 className=' text-gray-400 hover:text-orange-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <IoLogoCss3 className=' text-gray-400 h-7 w-7 hover:text-blue-500 duration-200 transition-all m-2' />
+                    <IoLogoJavascript className=' text-gray-400 hover:text-yellow-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <IoLogoSass className=' text-gray-400 hover:text-pink-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <SiTailwindcss className=' text-gray-400 hover:text-teal-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <SiTypescript className=' text-gray-400 hover:text-blue-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <IoLogoReact className=' text-gray-400 hover:text-cyan-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <SiRedux className=' text-gray-400 h-7 w-7 hover:text-purple-500 duration-200 transition-all m-2' />{' '}
+                    <IoLogoNodejs className=' text-gray-400 hover:text-green-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <SiExpress className=' text-gray-400 hover:text-gray-900 h-7 w-7 duration-200 transition-all m-2' />
+                    <SiNextdotjs className=' text-gray-400 hover:text-gray-900 h-7 w-7 duration-200 transition-all m-2' />
+                    <SiMongodb className=' text-gray-400 hover:text-green-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <SiSupabase className=' text-gray-400 hover:text-green-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <SiWebpack className=' text-gray-400 hover:text-cyan-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <SiPostcss className=' text-gray-400 hover:text-red-500 h-7 w-7 duration-200 transition-all m-2' />
+                    <SiGithub className=' text-gray-400 hover:text-gray-900 h-7 w-7 duration-200 transition-all m-2' />
+                    <IoGitMerge className=' text-gray-400  hover:text-orange-500 h-7 w-7 duration-200 transition-all m-2' />
                 </div>
             </div>
         </section>
