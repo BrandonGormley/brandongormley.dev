0570de99be3be2c264b1b8d548a69573e25bc319
Brandon Gormley <brandontgormley@gmail.com>
2024-02-10 20:03:12 -0600

    create template for skills page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
-
 export default function page() {
-    return <div>page</div>;
+    return (
+        <section className='w-full mx-auto max-w-[1280px]'>
+            <div className='flex flex-col my-12 justify-center mx-auto md:mx-0 md:flex-row md:justify-between max-w-[1280px] px-4 transition-all'>
+                <div className='flex flex-col w-full my-8 md:p-8 lg:p-16 md:justify-center lg:w-[65%]'>
+                    <p className='text-2xl font-light tracking-wide lg:my-2 lg:text-4xl leading-loose'>
+                        Skills
+                    </p>
+                    <p className='text-gray-800 text-lg font-light lg:my-4 leading-loose large:text-xl'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Commodi atque aliquid nesciunt, aspernatur facilis
+                        ratione modi odit cumque, autem eaque laudantium fugiat
+                        consequuntur quia tempore vero nulla, culpa non ex.
+                    </p>
+                </div>
+                <div></div>
+            </div>
+        </section>
+    );
 }
