383d431bb9cbcd8a9761a58ab0d75bcceda45dd4
Brandon Gormley <brandontgormley@gmail.com>
2024-02-16 13:58:25 -0600

    update styling on About component

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -10,14 +10,14 @@ export default function About() {
                     width={500}
                     height={500}
                     alt='hero image'
-                    className='rounded-lg'
+                    className='rounded-lg w-full md:max-w-[45%]'
                 />
 
                 <div className='flex flex-col w-full my-8 md:p-8 lg:p-16 md:justify-center'>
                     <p className='text-2xl font-light tracking-wide lg:my-2 lg:text-4xl leading-loose'>
                         Hello World! I&apos;m Brandon ☕️
                     </p>
-                    <p className='text-gray-800 text-3xl font-bold lg:my-2 lg:text-5xl xl:text-6xl leading-loose'>
+                    <p className='text-gray-800 text-3xl font-bold lg:my-2 lg:text-4xl xl:text-5xl leading-loose'>
                         Front End Developer
                     </p>
                     <p className='text-gray-800 text-lg font-light lg:my-4 leading-loose large:text-xl'>
