890f5508128d24f01cb70ad9bdcd5fa8df51ef72
Brandon Gormley <brandontgormley@gmail.com>
2024-02-16 14:01:04 -0600

    add _blank target for social icons

--- src/app/components/ui/SocialBar.tsx
+++ src/app/components/ui/SocialBar.tsx
@@ -12,22 +12,36 @@ export default function SocialBar() {
             <a
                 href='https://www.linkedin.com/in/brandontgormley/'
                 className='mr-2'
+                target='_blank'
             >
                 <IoLogoLinkedin className='hover:text-[#0077b5] text-gray-400 h-7 w-7 duration-200 transition-all' />
             </a>
-            <a href='https://www.instagram.com/brandongormley' className='mx-2'>
+            <a
+                href='https://www.instagram.com/brandongormley'
+                className='mx-2'
+                target='_blank'
+            >
                 <IoLogoInstagram className='hover:text-[#d62976] text-gray-400 h-7 w-7 duration-200 transition-all' />
             </a>
             <a
                 href='https://www.youtube.com/channel/UCOtIOhA9Y0a36ah9UUZ6RCw'
                 className='mx-2'
+                target='_blank'
             >
                 <IoLogoYoutube className='hover:text-[#FF0000] text-gray-400 h-7 w-7 duration-200 transition-all' />
             </a>
-            <a href='https://twitter.com/BrandonGormley' className='mx-2'>
+            <a
+                href='https://twitter.com/BrandonGormley'
+                className='mx-2'
+                target='_blank'
+            >
                 <IoLogoTwitter className='hover:text-[#1DA1F2] text-gray-400 h-7 w-7 duration-200 transition-all' />
             </a>
-            <a href='https://github.com/BrandonGormley' className='mx-2'>
+            <a
+                href='https://github.com/BrandonGormley'
+                className='mx-2'
+                target='_blank'
+            >
                 <IoLogoGithub className='hover:text-gray-900 text-gray-400 h-7 w-7 duration-200 transition-all' />
             </a>
         </div>
