5ae79d4744c9042ba1b1b84c3eabe9e5280b263c
Brandon Gormley <brandontgormley@gmail.com>
2024-02-24 13:26:41 -0600

    add basic opengraph info to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,20 @@ import './globals.css';
 import Header from '@/app/components/Header';
 
 export const metadata: Metadata = {
+    metadataBase: new URL('https://brandongormley.vercel.app/'),
     title: 'Brandon Gormley | Dev',
-    description: 'Welcome to my portfolio!',
+    description:
+        'Welcome to my digital hub—a place where creativity meets innovation. Explore my projects, skills, and insights as I journey through the ever-evolving world of development. Lets embark on this digital adventure together!',
+
+    openGraph: {
+        title: 'Brandon Gormley | Dev',
+        description:
+            'Welcome to my digital hub—a place where creativity meets innovation. Explore my projects, skills, and insights as I journey through the ever-evolving world of development. Lets embark on this digital adventure together!',
+        url: 'https://brandongormley.vercel.app/',
+        siteName: 'brandongormley.dev',
+        locale: 'en_US',
+        type: 'website',
+    },
 };
 
 export default function RootLayout({
